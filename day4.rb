4f6fdf93b3bd7030fe9d4e559877a12ea6ceff58
Alexander Korling <lermannen@gmail.com>
2015-12-07 22:58:22 +0100

    Solve 4b.

--- day4.rb
+++ day4.rb
@@ -1,11 +1,15 @@
 require 'digest'
 
-count = 1
-input = "bgvyzdsv"
-d = loop.each do
-  digest = Digest::MD5.hexdigest(input + count.to_s)
-  break(count) if digest.chars.take(5).all? { |i| i == "0" }
-  count += 1
+def find_digest(leading_zeroes)
+  input = "bgvyzdsv"
+  count = 1
+
+  loop.each do
+    digest = Digest::MD5.hexdigest(input + count.to_s)
+    break(count) if digest.chars.take(leading_zeroes).all? { |i| i == "0" }
+    count += 1
+  end
 end
 
-p d
+p find_digest(5)
+p find_digest(6)
